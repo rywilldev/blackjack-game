fb73aacdb93e0b3e3490a22936bed36f436e93b0
rywilldev <rywilldav@gmail.com>
2023-04-03 10:53:02 -0400

    add firstCard, secondCard, sum variables. add getRandomCard and checkCards functions

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-console.log("test");
-alert("hi!");
+const firstCard = getRandomCard(2, 11);
+const secondCard = getRandomCard(2, 11);
+let sum = firstCard + secondCard;
+console.log(firstCard);
+console.log(secondCard);
+console.log(sum);
+checkCards();
+function getRandomCard(min, max) {
+    const floatRandom = Math.random();
+    const difference = max - min;
+    const random = Math.round(difference * floatRandom);
+    const randomWithinRange = random + min;
+    return randomWithinRange;
+}
+
+function checkCards() {
+    if (sum < 21) {
+        console.log("Would you like to draw another card?");
+    } else if (sum === 21) {
+        console.log("You have blackjack! You win!");
+    } else {
+        console.log("You have gone over. You lose!");
+    }
+}
